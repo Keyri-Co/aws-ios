f84ebbde098a10e5c9de8fd322c7da1925f07996
Aditya Malladi <aditya@keyri.co>
2022-11-23 12:00:56 -0500

    initial

---
+++ AWSKeyri/AWSKeyri/AWSKeyriApp.swift
@@ -0,0 +1,17 @@
+//
+//  AWSKeyriApp.swift
+//  AWSKeyri
+//
+//  Created by Aditya Malladi on 11/23/22.
+//
+
+import SwiftUI
+
+@main
+struct AWSKeyriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
